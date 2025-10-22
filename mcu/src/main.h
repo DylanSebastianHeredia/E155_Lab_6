74b0cdc0e9250442ac7ed40f2621b7c98343a582
D. Sebastian Heredia <sebastianheredi4@gmail.com>
2025-10-22 14:40:55 -0700

    Update main.h

--- mcu/src/main.h
+++ mcu/src/main.h
@@ -10,7 +10,7 @@
 #include "STM32L432KC.h"
 #include "DS1722.h"         // Contains SPI1 macros
 
-#define LED_PIN     PB3     // LED pin for blinking on Port B pin 3
+#define LED_PIN     PB7     // LED pin for blinking
 #define BUFF_LEN    32      // Buffer length (ms)
 
 #endif // MAIN_H
